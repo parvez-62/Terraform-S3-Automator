9a80ff3acf26ec19990bf98890f5be692fbdfe5e
parvez-62 <prvzmusharaf@gmail.com>
2024-12-11 18:57:14 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,3 @@
-output "websiteendpoint" {
-  value = aws_s3_bucket.parvezbucket.website_endpoint 
+output "website_endpoint" {
+  value = aws_s3_bucket_website_configuration.website.website_domain
 }
